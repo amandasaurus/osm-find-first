b3b4d34b2bd0896cf2b902d3d51a8aceb36f6bdd
Rory McCann <rory@technomancy.org>
2014-12-14 16:47:36 +0100

    test empty

--- tests/test_osm_find_first.py
+++ tests/test_osm_find_first.py
@@ -77,5 +77,17 @@ class TestOsmFindFirst(unittest.TestCase):
         self.assertEqual(httpretty.last_request().headers['user-agent'], v("osm-find-first/{version}"))
 
 
+    @httpretty.activate
+    def testEmptyDoesNothing(self):
+        httpretty.register_uri(httpretty.GET,
+            re.compile("http://api.openstreetmap.org/api/0.6/(node|way|relation)/[0-9]+/1"),
+                               body='<osm><relation id="1" uid="123" user="testuser" timestamp="2000-01-01 115:24:02"></relation></osm>',
+            content_type="text/xml")
+
+        result = osm_find_first.find_first([], [])
+        self.assertEqual(result, [])
+        self.assertEqual(httpretty.last_request().__class__, httpretty.core.HTTPrettyRequestEmpty)
+
+
 if __name__ == '__main__':
     unittest.main()
