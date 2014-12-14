cd12a18634910b45655f9b66746930c8ca1d8377
Rory McCann <rory@technomancy.org>
2014-12-14 14:25:47 +0100

    pep8

--- osm_find_first/__init__.py
+++ osm_find_first/__init__.py
@@ -19,6 +19,7 @@ import os
 
 logger = logging.getLogger(__name__)
 
+
 def is_int(x):
     """Return True iff this can be turned into an int."""
     try:
@@ -27,34 +28,42 @@ def is_int(x):
     except:
         return False
 
+
 def is_osm_type(x):
     """Return True iff this is a valid 'osm_type'."""
     return x in ['node', 'way', 'relation']
 
-# osm_type ("node"/"way"/"relation"), osm_id, osm_uid, osm_user, osm_timestamp (ISO formatted)
+# osm_type ("node"/"way"/"relation"), osm_id, osm_uid, osm_user,
+# osm_timestamp (ISO formatted)
+
+
 def find_first(known_data, osm_objs):
     """
     Query OSM API for the objects in `osm_objs`.
-    
+
     Keeps in mind `known_data`, and returns the combination of `known_data` and the results for `osm_objs`.
     """
     if len(osm_objs) == 0:
         return known_data
 
     # ensure it's correct format
-    osm_objs = [{'osm_type': str(x['osm_type']), 'osm_id': str(x['osm_id'])} for x in osm_objs]
+    osm_objs = [{'osm_type': str(x['osm_type']), 'osm_id': str(x['osm_id'])}
+                for x in osm_objs]
 
-    known_data_indexed = dict(((x['osm_type'], x['osm_id']), x) for x in known_data)
-    missing_objs = [x for x in osm_objs if (x['osm_type'], x['osm_id']) not in known_data_indexed]
+    known_data_indexed = dict(((x['osm_type'], x['osm_id']), x)
+                              for x in known_data)
+    missing_objs = [x for x in osm_objs if (
+        x['osm_type'], x['osm_id']) not in known_data_indexed]
 
     num_objs = len(missing_objs)
     logger.info("Need to query OSM for %d objects", num_objs)
-    #print "Need to query OSM for %d objects" % num_objs
+    # print "Need to query OSM for %d objects" % num_objs
 
     for idx, obj in enumerate(missing_objs):
         assert is_osm_type(obj['osm_type'])
         assert is_int(obj['osm_id'])
-        url = "http://api.openstreetmap.org/api/0.6/{osm_type}/{osm_id}/1".format(**obj)
+        url = "http://api.openstreetmap.org/api/0.6/{osm_type}/{osm_id}/1".format(
+            **obj)
         response = requests.get(url, headers={'User-agent': USER_AGENT})
         time.sleep(0.1)
         parsed = ElementTree.fromstring(response.content)
@@ -63,12 +72,16 @@ def find_first(known_data, osm_objs):
         osm_uid = attrib['uid']
         osm_user = attrib['user']
         osm_timestamp = attrib['timestamp']
-        logger.debug("Obj %d of %d: Got details for %s %s", idx+1, num_objs, obj['osm_type'], obj['osm_id'])
-        #print "Obj %d of %d: Got details for %s %s" % ( idx+1, num_objs, obj['osm_type'], obj['osm_id'])
+        logger.debug("Obj %d of %d: Got details for %s %s",
+                     idx + 1, num_objs, obj['osm_type'], obj['osm_id'])
+        # print "Obj %d of %d: Got details for %s %s" % ( idx+1, num_objs,
+        # obj['osm_type'], obj['osm_id'])
 
-        known_data.append({'osm_id': obj['osm_id'], 'osm_type': obj['osm_type'], 'osm_uid': osm_uid, 'osm_user': osm_user, 'osm_timestamp':osm_timestamp})
+        known_data.append({'osm_id': obj['osm_id'], 'osm_type': obj[
+                          'osm_type'], 'osm_uid': osm_uid, 'osm_user': osm_user, 'osm_timestamp': osm_timestamp})
 
     return known_data
+
 
 def find_first_from_csvs(csv_known_filename, missing):
     """
@@ -80,7 +93,8 @@ def find_first_from_csvs(csv_known_filename, missing):
     with open(csv_known_filename) as fp:
         csv_known_reader = csv.DictReader(fp)
         known_data = list(csv_known_reader)
-        logger.info("Read %d OSM objects from file %s", len(known_data), csv_known_filename)
+        logger.info("Read %d OSM objects from file %s",
+                    len(known_data), csv_known_filename)
 
     if isinstance(missing, basestring) and os.path.isfile(missing):
         missing_osm_objs = read_missing_from_csv(missing)
@@ -91,19 +105,22 @@ def find_first_from_csvs(csv_known_filename, missing):
     try:
         new_data = find_first(known_data, missing_osm_objs)
     except BaseException as ex:
-        #print repr(ex)
+        # print repr(ex)
         # Exception!
         new_data = known_data
 
     write_to_csv(csv_known_filename, new_data)
     return new_data
 
+
 def write_to_csv(filename, result_data):
     """Write the result data to filename."""
     with open(filename, 'w') as fp:
-        csv_writer = csv.DictWriter(fp, ['osm_type', 'osm_id', 'osm_user', 'osm_uid', 'osm_timestamp'])
+        csv_writer = csv.DictWriter(
+            fp, ['osm_type', 'osm_id', 'osm_user', 'osm_uid', 'osm_timestamp'])
         csv_writer.writeheader()
         csv_writer.writerows(result_data)
+
 
 def read_missing_from_csv(csv_filename):
     """Read & return missing data from filename."""
@@ -111,6 +128,7 @@ def read_missing_from_csv(csv_filename):
         csv_reader = csv.DictReader(fp)
         missing = list(csv_reader)
     return missing
+
 
 def main(argv):
     """Main method."""
